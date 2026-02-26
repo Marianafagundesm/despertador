f05816080273db129b8fe5993de173d86a6cb1ce
Marianafagundesm <marimarcifagundes@gmail.com>
2026-02-26 19:43:53 -0300

    Add files via upload

---
+++ Subtrair.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Subtrair {  
+public static void main (String [] args) {
+    Scanner inputScanner = new Scanner (System.in) ;
+    System.out.println("Digite o primeiro número a ser subtraido e tecle Enter");
+    String respostaUsuario = inputScanner.nextLine().trim();
+    int numeroUm = Integer.valueOf(respostaUsuario);
+    System.out.println("Digite o segundo número a ser subtraido e tecle Enter");
+    respostaUsuario = inputScanner.nextLine().trim();
+    int numeroDois = Integer.valueOf(respostaUsuario);
+    System.out.println("o resultado da subtracao é:" + (numeroUm - numeroDois));
+    inputScanner.close();
+
+}   
+}
+    
+
